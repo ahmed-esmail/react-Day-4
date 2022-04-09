905278f61b41e90e2c78f5d15612e633badd300d
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2022-04-09 03:03:19 +0200

    cart and delete from cart

--- shop/src/components/product.jsx
+++ shop/src/components/product.jsx
@@ -1,28 +1,30 @@
-import {Link} from "react-router-dom";
-import {useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ProductsCRUD from "../ProductModel";
 import CartCRUD from "../CartModel";
+import { useState } from "react";
 
-
-function Product({prodItem}) {
+function Product({ prodItem }) {
+  let [selected, setSelected] = useState(false);
   const navigate = useNavigate();
   let navigateToEdit = () => {
     navigate(`product/edit/${prodItem.id}`);
   };
 
   let deleteById = (_id) => {
-    ProductsCRUD.deleteById(_id)
-  }
+    ProductsCRUD.deleteById(_id);
+  };
 
   let addToCart = (product) => {
-    CartCRUD.addProductToCart(product).then(res => console.log(res))
-  }
+    CartCRUD.addProductToCart(product).then((res) => console.log(res));
+    setSelected(true);
+  };
   return (
     <tr>
       <td>{prodItem.id}</td>
       <td>{prodItem.name}</td>
       <td>
-        <img src={prodItem.img} width="90px" alt="productImage"/>
+        <img src={prodItem.img} width="90px" alt="productImage" />
       </td>
       <td>${prodItem.price}</td>
       <td>{prodItem.category}</td>
@@ -30,19 +32,21 @@ function Product({prodItem}) {
       <td>
         <i
           className={"fa-solid fa-pencil"}
-          style={{cursor: "pointer"}}
+          style={{ cursor: "pointer" }}
           onClick={navigateToEdit}
         />
-        <i className="fas fa-shopping-cart ms-2"
-           style={{cursor: "pointer"}}
-           onClick={() => addToCart(prodItem)}/>
+
+        <i
+          className="fas fa-shopping-cart ms-2"
+          color="red"
+          style={{ cursor: "pointer" }}
+          onClick={() => addToCart(prodItem)}
+        />
         <i
           className="fa-solid fa-trash ms-2 "
-          style={{cursor: "pointer"}}
+          style={{ cursor: "pointer" }}
           onClick={() => deleteById(prodItem.id)}
-
         />
-
       </td>
     </tr>
   );
